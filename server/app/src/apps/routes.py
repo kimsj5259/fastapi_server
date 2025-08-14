012b5d7ea64ea795e528e4e89023bc13c284ee2d
jinlaiv <jinlaiv@jinlaivui-MacBookPro.local>
2025-08-14 09:26:15 +0900

    fix: views to name routers

--- server/app/src/apps/routes.py
+++ server/app/src/apps/routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 
-from .views import journal_view, mood_view, auth_view, role_view, user_view, community_view
+from .routers import journal_router, mood_router, auth_router, role_router, user_router, community_router
 
 
 api_router = APIRouter()
 
-api_router.include_router(auth_view.router, prefix="/auth", tags=["auth"])
-api_router.include_router(role_view.router, prefix="/role", tags=["role"])
-api_router.include_router(user_view.router, prefix="/user", tags=["user"])
-api_router.include_router(journal_view.router, prefix="/journal", tags=["journal"])
-api_router.include_router(mood_view.router, prefix="/mood", tags=["mood"])
-api_router.include_router(community_view.router, prefix="/community", tags=["community"])
+api_router.include_router(auth_router.router, prefix="/auth", tags=["auth"])
+api_router.include_router(role_router.router, prefix="/role", tags=["role"])
+api_router.include_router(user_router.router, prefix="/user", tags=["user"])
+api_router.include_router(journal_router.router, prefix="/journal", tags=["journal"])
+api_router.include_router(mood_router.router, prefix="/mood", tags=["mood"])
+api_router.include_router(community_router.router, prefix="/community", tags=["community"])
